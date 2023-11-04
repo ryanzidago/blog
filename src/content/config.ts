dff0b8e42ec9fd7dc209b2db9b54d4dd35e97a65
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-11-04 20:21:01 +0100

    Added TILs

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,4 +12,16 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const til = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, til };
